2127d4b36016206d5898d5d030ae110cd3e41444
Sriram Reddy Koonadi <sriramreddykoonadi@gmail.com>
2024-06-11 11:21:05 +0530

    Time: 58 ms (5.48%), Space: 16.5 MB (36.93%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        j=1
+        d={}
+        for i in range(97,123):
+            d[chr(i)]=j
+            j=j+1
+        ns=""
+        for i in s:
+            ns=ns+str(d.get(i))
+        ns=int(ns)
+        s=0
+        while k!=0:
+            rem=0
+            s=0
+            while ns>0:
+                rem=ns%10
+                s=s+rem
+                ns=ns//10
+            ns=s
+            k=k-1
+        return ns
