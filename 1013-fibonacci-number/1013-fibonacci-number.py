2312c4127d472803bfaa6157ebb238ffc8676019
Sriram Reddy Koonadi <sriramreddykoonadi@gmail.com>
2024-06-16 23:11:40 +0530

    Time: 367 ms (10.58%), Space: 16.4 MB (94.6%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def f(n):
+            if n==0:
+                return 0
+            elif n==1:
+                return 1
+            else:
+                return f(n-1)+f(n-2)
+        r=f(n)
+        return r
