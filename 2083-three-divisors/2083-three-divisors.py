a0fb1ca7f9135eb0ea9b6e37d22141d197b84a51
Sriram Reddy Koonadi <sriramreddykoonadi@gmail.com>
2024-06-20 07:48:57 +0530

    Time: 33 ms (83.16%), Space: 16.4 MB (84.82%) - LeetHub

---
+++ 2083-three-divisors/2083-three-divisors.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        c=0
+        for i in range(1,n+1):
+            if n%i==0:
+                c=c+1
+        if c==3:
+            return True
+        else:
+            return False
