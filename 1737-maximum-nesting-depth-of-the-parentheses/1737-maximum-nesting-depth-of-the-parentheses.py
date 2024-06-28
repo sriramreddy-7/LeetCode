0e3f2e26af071b4d782a4898bb01bc1d89dbe3cf
Sriram Reddy Koonadi <sriramreddykoonadi@gmail.com>
2024-06-28 20:42:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
@@ -4,8 +4,8 @@ class Solution:
         c=0
         for i in s:
             if i=="(":
-                c=c+1
+                r.append(c)
+                c=0
             elif i==")":
-                c=c-1
-            r.append(c) 
-        return max(r) 
+                c=c+1 
+        return c 
