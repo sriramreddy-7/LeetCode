43caa1782b04bdf1f2e00a478c861de6aca89a42
Sriram Reddy Koonadi <sriramreddykoonadi@gmail.com>
2024-07-17 01:32:55 +0530

    Time: 36 ms (66.53%), Space: 16.5 MB (66.78%) - LeetHub

---
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        b="balon"
+        d={}
+        for i in text:
+            if i in b:
+                if i not in d:
+                    d[i]=1
+                else:
+                    d[i]+=1
+        def check():
+            if len(d)<5:
+                return 0
+            c = set()
+            for k,v in d.items():
+                flag=0
+                if k=="o" or k=="l":
+                    if v>=2:
+                        if v%2==0:
+                            c.add(v//2)
+                        else:
+                            c.add((v-1)//2)
+                    else:
+                        return 0
+                else:
+                    c.add(v)
+            return min(c)
+        return check()
